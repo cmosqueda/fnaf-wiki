98180905ccdde87680c4580023697cc9fa54c06d
tyne <mosqueda.christinereisa04@gmail.com>
2025-05-18 17:42:21 +0800

    not found modified

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,11 +1,18 @@
 import { Stack } from "expo-router";
 import { Text, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Link } from "expo-router";
 
 export default function NotFound() {
   return (
     <>
-      <View className="flex flex-grow items-center justify-center align-middle">
-        <Text className="font-bold text-6xl text-red-500">Page not found.</Text>
+      <View className="flex flex-grow items-center justify-center align-middle space-y-5">
+        <MaterialIcons name="error-outline" size={50} className="text-red-500"></MaterialIcons>
+        <Text className="font-bold text-4xl text-red-500 text-center">Page not found.</Text>
+        <Text>The path you requested did not match any of the defined routes</Text>
+        <Link href={"/"} className="italic hover:underline">
+          Go back to Home
+        </Link>
       </View>
     </>
   );
